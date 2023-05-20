e571e32ac254cd02ee22b98bfb28b9800c2c68aa
Hanssell89 <hanssell.llanes16143@est.uca.edu.ni>
2023-05-19 18:33:53 -0600

    Se agrego el programa 8

---
+++ programa8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int suma = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+            suma += i;
+        }
+    }
+
+    cout << "La suma de los numeros pares del 1 al 100 es: " << suma << endl;
+
+    return 0;
+}
