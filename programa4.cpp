b6d3ae9fc490fcbad91e53fbfa6c30c6851c2887
Hanssell89 <hanssell.llanes16143@est.uca.edu.ni>
2023-05-19 18:19:12 -0600

    Se agrego el programa 4

---
+++ programa4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+bool esPrimo(int numero) {
+    if (numero <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= numero; ++i) {
+        if (numero % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int numero;
+
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+
+    if (esPrimo(numero)) {
+        cout << "El numero es primo." << endl;
+    } else {
+        cout << "El numero no es primo." << endl;
+    }
+
+    return 0;
+}
